10743bb839f26583c37a570ed7a5e48a43198601
varun gupta <varung735@gmail.com>
2023-03-16 00:18:50 +0530

    made changes to cookie

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -29,6 +29,7 @@ exports.login = async (req, res) => {
 
             const options = {
                 expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+                secure: true,
                 httpOnly: true
             }
 
