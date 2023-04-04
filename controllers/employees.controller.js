a7355d5a1dad25027feb621e88670acbad3fc21e
varun gupta <varung735@gmail.com>
2023-04-04 23:07:29 +0530

    set secure value to false

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -31,7 +31,7 @@ exports.login = async (req, res) => {
 
             const options = {
                 expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-                secure: true,
+                secure: false,
                 httpOnly: true,
                 sameSite: "lax"
             }
